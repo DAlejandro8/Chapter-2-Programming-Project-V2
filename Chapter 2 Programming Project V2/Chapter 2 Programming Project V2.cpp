17723f9a6fff7a5e572b4035f171176161c32f60
legod <legod@BRICK>
2025-06-23 23:51:21 -0400

    Comments

--- Chapter 2 Programming Project V2/Chapter 2 Programming Project V2.cpp
+++ Chapter 2 Programming Project V2/Chapter 2 Programming Project V2.cpp
@@ -9,18 +9,19 @@ int main()
 
     double  people = 16500;
 
-   double purchase = 0.15;
+   double purchase = 0.15;//Converted from a percent
 
-   double citrus = 0.58;
+   double citrus = 0.58;//Converted from a percent
 
     
-   double ppurchase = people * purchase;
-    double pcitrus = ppurchase * citrus;
+   double ppurchase = people * purchase;//individual people
+    double pcitrus = ppurchase * citrus;//individual peopele.
+    //I used the assignment as a part of the calculation for pcitrus
     
     
 
 
-
+    //output
 
     std::cout << "The approximate number of customers in the survey purchasing one or more energy drinks per week is " <<ppurchase<< std::endl;
     std::cout << "The approximate number of customers in the survey preferring citrus-flavored energy drinks is " <<pcitrus<< std::endl;
