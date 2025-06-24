e55c24952ac8d7c8b7f6c45f7ea0d4e69ea25ef2
legod <legod@BRICK>
2025-06-23 23:06:13 -0400

    Only needs comments

--- Chapter 2 Programming Project V2/Chapter 2 Programming Project V2.cpp
+++ Chapter 2 Programming Project V2/Chapter 2 Programming Project V2.cpp
@@ -5,16 +5,33 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+ 
+
+    double  people = 16500;
+
+   double purchase = 0.15;
+
+   double citrus = 0.58;
+
+    
+   double ppurchase = people * purchase;
+    double pcitrus = ppurchase * citrus;
+    
+    
+
+
+
+
+    std::cout << "The approximate number of customers in the survey purchasing one or more energy drinks per week is " <<ppurchase<< std::endl;
+    std::cout << "The approximate number of customers in the survey preferring citrus-flavored energy drinks is " <<pcitrus<< std::endl;
+
+   
+
+
+
+
+
+
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
