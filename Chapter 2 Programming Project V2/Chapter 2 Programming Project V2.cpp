61f72164929f1e211de1e41d7e532075a6a80be1
legod <legod@BRICK>
2025-06-23 23:49:05 -0400

    Comments added

--- Chapter 2 Programming Project V2/Chapter 2 Programming Project V2.cpp
+++ Chapter 2 Programming Project V2/Chapter 2 Programming Project V2.cpp
@@ -5,21 +5,21 @@
 
 int main()
 {
-    double  Gallons = 20;
+    double  Gallons = 20;// The tank size of the car, in gallons.
 
-    double Mileaget = 23.5;
+    double Mileaget = 23.5;//mileage in a town
 
-    double Mileageh = 28.9;
+    double Mileageh = 28.9;//mileage on the highway
   
-    double Distancet = Mileaget * Gallons;
-    double Distanceh = Mileageh * Gallons;
+    double Distancet = Mileaget * Gallons;//Units in miles 
+    double Distanceh = Mileageh * Gallons;//Units in miles
     
 
 
 
 
  
-
+    //Output
     std::cout << "The distance, in miles, the car can travel on one tank of gas when driven in town is " << Distancet <<  std::endl;
     std::cout << "The distance, in miles, the car can travel on one tank of gas when driven on the highway is " << Distanceh <<  std::endl;
 
