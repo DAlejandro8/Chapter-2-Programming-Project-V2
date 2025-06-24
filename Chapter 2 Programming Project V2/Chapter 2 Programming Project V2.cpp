78ca13b1e1342912b1461a94f69a2865cd42e57c
legod <legod@BRICK>
2025-06-23 23:46:16 -0400

    Comments

--- Chapter 2 Programming Project V2/Chapter 2 Programming Project V2.cpp
+++ Chapter 2 Programming Project V2/Chapter 2 Programming Project V2.cpp
@@ -8,13 +8,13 @@ int main()
     
    
 
-   double  Rate = 1.5 ;
+   double  Rate = 1.5 ; //The rate the ocean is rising per year
 
-   int Year5 = 5;
+   int Year5 = 5;//In five years
 
-   int Year7 = 7;
+   int Year7 = 7;//In 7 years
 
-   int Year10 = 10 ;
+   int Year10 = 10 ;//In 10 years
 
    double Level5 = Year5 * Rate;
    double Level7 = Year7 * Rate;
@@ -22,7 +22,7 @@ int main()
 
 
 
-
+   //output results
     std::cout << "In " << Year5 << " years, the ocean will be " << Level5 << " millimeters higher \n";
     std::cout << "In " << Year7 << " years, the ocean will be " << Level7 << " millimeters higher \n";
     std::cout << "In " << Year10 << " years, the ocean will be " << Level10 << " millimeters higher \n";
