acbc599b0f38403a200289fc931589842213eeda
legod <legod@BRICK>
2025-06-23 22:39:19 -0400

    Quick Save

--- Chapter 2 Programming Project V2/Chapter 2 Programming Project V2.cpp
+++ Chapter 2 Programming Project V2/Chapter 2 Programming Project V2.cpp
@@ -5,16 +5,25 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double  Gallons = 20;
+
+    double Mileaget = 23.5;
+
+    double Mileageh = 28.9;
+  
+    double Distancet = Mileaget * Gallons;
+    double Distanceh = Mileageh * Gallons;
+    
+
+
+
+
+ 
+
+    std::cout << "The distance the car can travel on one tank of gas when driven in town is " << Distancet << " miles." std::endl;
+    std::cout << "The distance the car can travel on one tank of gas when driven on the highway is " << Distanceh << " miles." std::endl;
+
+
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
